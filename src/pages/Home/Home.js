31ec2300283fd9fb24ee3860a0d19cafed949e6f
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-10-11 15:37:56 -0300

    getting posts from db

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,23 +4,33 @@ import styles from './Home.module.css'
 
 import { useNavigate, Link } from 'react-router-dom'
 import { useState } from 'react'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 
 // components
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts, setPosts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault()
   }
   return (
     <div className={styles.home}>
-      <h1>Posts recentes</h1>
       <form onSubmit={handleSubmit} className='styles.search_form'>
-        <input type="text" placeholder='Pesquisar post' value={query} onChange={(e) => setQuery(e.target.value)}/>
+        <input type="text" placeholder='Pesquisar post' value={query} onChange={(e) => setQuery(e.target.value)} />
         <button className="btn btn-dark">Pesquisar</button>
       </form>
+      <h1>Posts recentes</h1>
+      {posts && posts.map((post) => (
+        <>
+          <h2>{post.title}</h2>
+          <img src={post.image}></img>
+          <p>{post.description}</p>
+          <p>{post.tagsArray}</p>
+        </>
+      ))}
+      
       <div>
         {/* Criar validação de usuário logado */}
         {posts && posts.length === 0 && (
