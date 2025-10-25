276163c948b2010739579614b11dc1e852e65b57
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-10-24 21:53:45 -0300

    creating dashboard code

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,32 @@
 import styles from "./Dashboard.module.css"
+import { Link } from "react-router-dom"
+
+// hooks
+import { useAuthValue } from "../../context/AuthContext"
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+
 
 const Dashboard = () => {
+    const { user } = useAuthValue()
+    const uid = user.uid;
+
+    // Posts do usuário
+    const posts = [];
+
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h2>Dashboard</h2>
+            <p>Gerencie suas publicações</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noposts}>
+                    <p>Você ainda não tem publicações.</p>
+                    <Link to="/posts/new/" className="btn">Criar meu primeiro post</Link>
+                </div>
+            ) : (
+                <div>
+
+                </div>
+            )}
         </div>
     )
 }
